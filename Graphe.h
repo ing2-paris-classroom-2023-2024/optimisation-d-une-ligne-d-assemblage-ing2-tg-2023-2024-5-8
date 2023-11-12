f3fc19775c782542575f6a76770835c7a35a8fce
margauxvaret <132448417+margauxvaret@users.noreply.github.com>
2023-11-12 19:02:05 +0100

    Add files via upload

---
+++ Graphe.h
@@ -0,0 +1,43 @@
+#ifndef TP6_DUFLOT_VARET_GRAPHE_H
+#define TP6_DUFLOT_VARET_GRAPHE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+struct Arc {
+    int sommet;
+    int capacite;
+    int flot;
+    struct Arc* arc_suivant;
+};
+
+typedef struct Arc* pArc;
+
+struct Sommet {
+    struct Arc* arc;
+    int valeur;
+    char nom_sommet[2]; //modifier la taille du tableau si nécessaire
+    int flux_min; //pour stocker le flux minimal du chemin augmentant
+};
+
+typedef struct Sommet* pSommet;
+
+typedef struct Graphe {
+    int taille;
+    int ordre;
+    pSommet* pSommet;
+} Graphe;
+
+typedef struct {
+    int sommet_depart, sommet_arrive, poids;
+} Arete;
+
+struct Pile {
+    pSommet sommets[15];
+    int sommetCourant;
+};
+
+
+#endif //TP6_DUFLOT_VARET_GRAPHE_H
