0374decc1c7cf044ec5bfa338d3c96b44a863205
margauxvaret <132448417+margauxvaret@users.noreply.github.com>
2023-11-13 19:14:12 +0100

    Update Graphe.h
    mise à jour fonction

--- Graphe.h
+++ Graphe.h
@@ -7,29 +7,6 @@
 #include <string.h>
 
 ///--------------------------------------------STRUCTURES-------------------------------------------------
-struct Arc {
-    int sommet;
-    int capacite;
-    int flot;
-    struct Arc* arc_suivant;
-};
-
-typedef struct Arc* pArc;
-
-struct Sommet {
-    struct Arc* arc;
-    int valeur;
-    char nom_sommet[2]; //modifier la taille du tableau si nécessaire
-    int flux_min; //pour stocker le flux minimal du chemin augmentant
-};
-
-typedef struct Sommet* pSommet;
-
-typedef struct Graphe {
-    int taille;
-    int ordre;
-    pSommet* pSommet;
-} Graphe;
 
 typedef struct {
     int sommet_depart, sommet_arrive, poids;
@@ -39,18 +16,6 @@ struct Pile {
     pSommet sommets[15];
     int sommetCourant;
 };
-
-///--------------------------------------------FONCTIONS-------------------------------------------------
-//fonction pour afficher les successeurs du sommet num
-void afficher_successeurs(pSommet* sommet, int num);
-
-//fonction pour créer le graphe
-Graphe* CreerGraphe(int ordre);
-
-//fonction pour lire le graphe depuis un fichier
-Graphe* lire_graphe(char* nomFichier);
-
-
 // structure pour stocker une composante connexe
 struct ComposanteConnexe
 {
@@ -60,5 +25,76 @@ struct ComposanteConnexe
 
 // alias de pointeur sur une composante connexe
 typedef struct ComposanteConnexe* pComposanteConnexe;
+// structure pour stocker une composante connexe
+struct ComposanteConnexe
+{
+    int* sommets; // tableau des sommets dans la composante
+    int taille;   // nombre de sommets dans la composante
+};
+
+// alias de pointeur sur une composante connexe
+typedef struct ComposanteConnexe* pComposanteConnexe;
+
+/* structure pour stocker un chemin */
+struct Chemin
+{
+    int sommet;
+    struct Chemin* suivant;
+};
+
+/* alias de pointeur sur un Chemin */
+typedef struct Chemin* pChemin;
+
+/* structure d'un arc*/
+struct Arc
+{
+    int sommet; // numéro de sommet d'un arc adjacent au sommet initial
+    int valeur;
+    struct Arc* arc_suivant;
+};
+
+/* alias de pointeur sur un Arc */
+typedef struct Arc* pArc;
+
+/* structure d'un sommet*/
+struct Sommet
+{
+    struct Arc* arc;
+    int valeur;
+    char couleur; //remplacé par visite
+};
+
+/* alias de pointeur sur un Sommet */
+typedef struct Sommet* pSommet;
+
+/* alias d'un Graphe */
+typedef struct Graphe
+{
+    int taille;
+    int orientation;
+    int ordre;
+    pSommet* pSommet;
+} Graphe;
+
+///--------------------------------------------FONCTIONS-------------------------------------------------
+
+// créer le graphe
+Graphe* CreerGraphe(int ordre);
+
+// lire le graphe
+Graphe * lire_graphe(char * nomFichier);
+
+// ajouter l'arête entre les somets s1 et s2 du graphe
+pSommet* CreerArete(pSommet* sommet,int s1,int s2, int premierSommet);
+
+/* affichage des successeurs du sommet num*/
+void afficher_successeurs(pSommet * sommet, int num);
+
+/*affichage du graphe avec les successeurs de chaque sommet */
+void graphe_afficher(Graphe* graphe);
+
+
+
+
 
 #endif //PROJET_GRAPHE_H 
